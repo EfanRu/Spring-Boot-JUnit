b1d656d126b74343cf12c616d3cc92c26c94a53a
Vyacheslav <ffgeniy@mail.ru>
2020-02-23 17:46:09 +0300

    Prepare project

---
+++ src/main/java/com/example/spring_junit/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.example.spring_junit.dao;
+
+import com.example.spring_junit.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User> getAllUsers();
+    boolean addUser(User u);
+    boolean deleteUser(String id);
+    boolean updateUser(String id, String firstName, String lastName, String phoneNumber, String role, String login, String password);
+    boolean updateUser(String id, String firstName, String lastName, String phoneNumber, String role, String login);
+    boolean updateUser(User user);
+    boolean checkAuth(String login, String password);
+    User getUserByLogin(String login);
+    User getUserById(String id);
+}
---
+++ src/main/java/com/example/spring_junit/security/DefaultUserAndAdmin.java
@@ -0,0 +1,42 @@
+package com.example.spring_junit.security;
+
+import com.example.spring_junit.model.Role;
+import com.example.spring_junit.model.User;
+import com.example.spring_junit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:application.properties")
+@Order(3)
+public class DefaultUserAndAdmin {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private Environment env;
+    
+    @Bean
+    public void DefaultUserAndAdmin() {
+        User user = new User();
+        user.setFirstName("Default user");
+        user.setLastName("Default user");
+        user.setPhoneNumber(99L);
+        user.setLogin(env.getProperty("db.default.user.login"));
+        user.setPassword(env.getProperty("db.default.user.password"));
+        user.setRole(new Role("user"));
+        userService.addUser(user);
+
+        User admin = new User();
+        admin.setFirstName("Default admin");
+        admin.setLastName("Default admin");
+        admin.setPhoneNumber(99L);
+        admin.setLogin(env.getProperty("db.default.admin.login"));
+        admin.setPassword(env.getProperty("db.default.admin.password"));
+        admin.setRole(new Role("admin"));
+        userService.addUser(admin);
+    }
+}
